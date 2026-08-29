ee7e56c156535c598f7208b5fa20c8839092c0ec
Albert1108o <albertoguilherme772@gmail.com>
2026-08-29 12:40:39 -0300

    removendo o html e adiconando o index.php

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
-<html lang="pt_BR">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Site de perfume</title>
+    <title>Document</title>
 </head>
 <body>
     
