5dd3ff16acac65cb7f14259c018431bdbac7b60e
Biankapn <nascimentobianka92@gmail.com>
2026-09-02 13:33:45 -0300

    codigofunconal

--- index.php
+++ index.php
@@ -6,6 +6,28 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <!--cabeçalho do site-->
+    <header>
+        <div class="titulo">
+ <h1>
+     Brand'Collection
+ </h1>
+</div>
+<div class="pesquisa">
+ <form class="search">
+    <input type="text" placeholder="Perfumes..." />
+    <button type="submit">🔍</button>
+  </form>
+</div>
+<!--links-->
+<div class="links">
+    <ul>
+      <a><li>Novidades</li></a>
+         <a><li>Sobre nós</li></a>
+          <a><li>Cadastre-se</li></a>
+           <a><li>Contate-nos</li></a>
+    </ul>
+</div>
+    </header>
 </body>
 </html>
