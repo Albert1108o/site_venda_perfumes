48f76cf646e29a5465ed94d24440383359e484f8
Biankapn <nascimentobianka92@gmail.com>
2026-09-03 13:06:58 -0300

    cabecalho e rodape

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,6 +9,38 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </head>
 <body>
-    
+    <!--cabeçalho do site-->
+    <header>
+
+        <h1>WEPINK</h1>
+
+        <nav>
+            <a href="#novidades">Novidades</a>
+            <a href="#sobre">Sobre nós</a>
+            <a href="#contato">Contate-nos</a>
+        </nav>
+
+        <form>
+            <input 
+                type="search" 
+                name="pesquisa" 
+                placeholder="Pesquisar..."
+            >
+            <button type="submit">Pesquisar</button>
+        </form>
+
+    </header>
+
+    <!--rodapé-->
+    <footer>
+        <p>&copy; 2026 WEPINK. Todos os direitos reservados.</p>
+        <!--links do --rodapé-->
+        <nav>
+        <a href="#novidades">Instagram</a>
+        <a href="#sobre">whatsapp</a>
+        <a href="#contato">Loja física</a>
+    </nav>
+
+    </footer>
 </body>
 </html>
