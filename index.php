4d81ec1f9969b023d2b330ba075fa849dcc9e064
Biankapn <nascimentobianka92@gmail.com>
2026-09-03 12:55:34 -0300

    atualiza

--- index.php
+++ index.php
@@ -4,30 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </head>
 <body>
-    <!--cabeçalho do site-->
-    <header>
-        <div class="titulo">
- <h1>
-     Brand'Collection
- </h1>
-</div>
-<div class="pesquisa">
- <form class="search">
-    <input type="text" placeholder="Perfumes..." />
-    <button type="submit">🔍</button>
-  </form>
-</div>
-<!--links-->
-<div class="links">
-    <ul>
-      <a><li>Novidades</li></a>
-         <a><li>Sobre nós</li></a>
-          <a><li>Cadastre-se</li></a>
-           <a><li>Contate-nos</li></a>
-    </ul>
-</div>
-    </header>
+    
 </body>
 </html>
