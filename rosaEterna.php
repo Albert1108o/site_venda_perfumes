83a12b7f5660f045ab676ebc28eb4cad17d8b997
jennifer <jennifer@email.com>
2026-09-04 23:03:40 -0300

    criei o arquivo rosaEterna.php e adicionei o cabeçalho do site e conteudo principal

---
+++ rosaEterna.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+      <!--cabeçalho do site-->
+     <header>
+
+        <h1>WEPINK</h1>
+
+        <nav>
+            <a href="#novidades">Novidades</a>
+            <a href="#sobre">Sobre nós</a>
+            <a href="#contato">Contate-nos</a>
+        </nav>
+
+        <form>
+            <input 
+                type="search" 
+                name="pesquisa" 
+                placeholder="Pesquisar..."
+            >
+            <button type="submit">Pesquisar</button>
+        </form>
+
+    </header>
+
+    <!-- conteúdo principal -->
+    <main>
+        <div class="container">
+
+            <div class="imagem">
+                <img src="...">
+            </div>
+
+            <div class="descricao">
+                <h2>Rosa Eterna</h2>
+                <p>Uma fragrância que combina a doçura da rosa com a elegância do jasmim.</p>
+                <p>R$ 489,00</p>
+            </div>    
+
+            <div class="botoes">
+                <button>Comprar</button>
+                <button>Adicionar ao carrinho</button>
+            </div>
+        </div>
+
+    </main>
+
+</body>
+</html>
